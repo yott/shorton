f69825eb267f9602600391c5bcdef40c1ac30224
Logan Yott <loganyott@gmail.com>
2016-03-31 12:41:10 -0600

    Updating plugin name

--- shortcode.php
+++ shortcode.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Plugin Name: Shortcode
+ * Plugin Name: Shorton
  * Description: This plugin allows you to define shortcodes via JSON and enable WP integrations for them.
  * Plugin URI: https://github.com/yott/wp-shortcode
  * Author: Logan Yott
